668ac3de3a2d129e9a6e6f56abded612f2053c8f
piptouque <piptouque@protonmail.com>
2021-07-02 16:58:51 +0200

    [+]: export of svelte components in UI namespace.

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,11 @@ export * from './core';
 export * from './components';
 export * from './layouts';
 export * from './utils';
+
+// Export all svelte components in './ui' in a new namespace UI.
+// e.g. Button becomes UI.Button.
+// This is done to prevent ambiguity with Marcelle components in './components'.
+// see: https://stackoverflow.com/a/47501654
+
+import * as UI from './ui';
+export { UI }
